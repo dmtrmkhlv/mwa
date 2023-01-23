19fb891a4ea6a18cefee527ab14162ae5ef593e5
Максим Кукушкин <maxkuku@gmail.com>
2023-01-23 18:05:48 +0300

    Хедер главной страницы
    Почти вёрстка

--- front/src/page/MainPage.tsx
+++ front/src/page/MainPage.tsx
@@ -1,9 +1,28 @@
+import { Button } from "@mui/material";
+import { Container } from "@mui/system";
 import Layout from "../modules/layout/Layout";
+import { Header } from "./Header/Header";
 
 export const MainPage = () => {
   return (
     <Layout>
-      <h1>Главная</h1>
+      <Header/>
+
+      <Container>
+        <div className='main_cont-sides'>
+          <div>
+            <Container>
+              <h1>Расскажите родным и друзьям, о чём мечтаете!</h1>
+            </Container>
+            <Button href={'/create'}>Создать список</Button>
+          </div>
+          <div>
+            <img src={'../images/brown_lady.jpg'} alt='Brown Lady'/>
+          </div>
+        </div>
+        
+      </Container>
+
     </Layout>
   );
 };
